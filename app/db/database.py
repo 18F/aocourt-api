0c5aa303e65bcf231d2052a84a13bc423b2f3c4c
Mark Meyer <mark@photo-mark.com>
2021-07-23 10:17:42 -0800

    add sqlite example that works with tests

--- app/db/database.py
+++ app/db/database.py
@@ -8,8 +8,16 @@ from app.core.config import settings
 uri = settings.DATABASE_URL
 
 engine = create_engine(uri)
+
 # Note: use "check_same_thread" for SQLite. ie:
-# settings.DATABASE_URL, connect_args={"check_same_thread": False}, echo=True
+'''
+from sqlalchemy.pool import StaticPool
+engine = create_engine(
+    'sqlite:///:memory:',
+    connect_args={'check_same_thread': False},
+    poolclass=StaticPool, echo=True
+)
+'''
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
