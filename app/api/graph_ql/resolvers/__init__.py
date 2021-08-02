b6c2ca35ce602143a86b8a607e4147039e8b3703
Mark Meyer <mark@photo-mark.com>
2021-08-02 13:24:09 -0800

    Adds the ability to list lower courts of a given higher court

--- app/api/graph_ql/resolvers/__init__.py
+++ app/api/graph_ql/resolvers/__init__.py
@@ -1,6 +1,7 @@
 from .case import case, docketentry
+from .court_resolvers import court
 from .query import query
 from .mutations import mutation
 from .dev_helpers import dev_mutation
 
-resolvers = [query, mutation, dev_mutation, case, docketentry]
+resolvers = [query, mutation, dev_mutation, case, court, docketentry]
