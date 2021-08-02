388d7d8f6736f8e93cad39e4af0a5de946864511
Mark Meyer <mark.meyer@gsa.gov>
2021-08-02 12:50:09 -0800

    Revert "Readme fix"

--- app/api/graph_ql/resolvers/__init__.py
+++ app/api/graph_ql/resolvers/__init__.py
@@ -1,7 +1,6 @@
 from .case import case, docketentry
-from .court_resolvers import court
 from .query import query
 from .mutations import mutation
 from .dev_helpers import dev_mutation
 
-resolvers = [query, mutation, dev_mutation, case, court, docketentry]
+resolvers = [query, mutation, dev_mutation, case, docketentry]
